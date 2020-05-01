ecc92f1738bab6c377f47495ab948f5c7efa3b56
Gabriel Zsigmond <gaazsig@gmail.com>
2020-05-01 12:53:39 -0300

    added wrapper to global style

--- src/resources/globalStyle.js
+++ src/resources/globalStyle.js
@@ -1,7 +1,26 @@
 import styled from "styled-components";
 
 const fullPageWrapper = styled.div`
-  min-height: 100vh;
+  min-height: calc(100vh - 210px);
 `;
 
-export { fullPageWrapper };
+const Wrapper = styled.div`
+  box-sizing: content-box;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 40px;
+  padding-right: 40px;
+
+  @media (max-width: 960px) {
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+
+  @media (max-width: 640px) {
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+`;
+
+export { fullPageWrapper, Wrapper };
