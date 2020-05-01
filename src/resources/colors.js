d8472d906ed48bf954bbdb5c84f08701e35d2ecd
Gabriel Zsigmond <gaazsig@gmail.com>
2020-05-01 02:26:46 -0300

    added colors and global style to resources

---
+++ src/resources/colors.js
@@ -0,0 +1,8 @@
+const darkGrey = "#4F4F4F";
+const lightGrey = "#ECECEC";
+const greenGradient = "90deg, #12C1C5 0%, #4CDE92 100%";
+const greenGradientInvert = "270deg, #12C1C5 0%, #4CDE92 100%";
+const BlueGradient =
+  "90deg, rgba(120, 142, 255, 0.9) 0%, rgba(36, 113, 255, 0.9) 98.96%";
+
+export { darkGrey, lightGrey, greenGradient, greenGradientInvert, BlueGradient };
---
+++ src/resources/globalStyle.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const fullPageWrapper = styled.div`
+  min-height: 100vh;
+`;
+
+export { fullPageWrapper };
