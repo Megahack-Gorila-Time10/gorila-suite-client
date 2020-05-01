1f7b0982b72bc13067a1d6b3e8cdabb8e67beb50
Gabriel Zsigmond <gaazsig@gmail.com>
2020-05-01 02:25:54 -0300

    created global index for components and containers

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header";
+import InputBox from "./InputBox";
+import Button from "./Button";
+import Form from "./Form";
+import ColorFooter from "./ColorFooter";
+
+export { Button, ColorFooter, Form, Header, InputBox };
