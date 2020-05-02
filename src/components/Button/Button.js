6e30e3958f3ec3274ca207cc2490e77393b819bf
Gabriel Zsigmond <gaazsig@gmail.com>
2020-05-02 02:12:33 -0300

    button can now have a callback func

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,7 +3,11 @@ import * as S from "./style";
 
 class Button extends Component {
   render() {
-    return this.props.lg ? <S.BtnLG>{this.props.text}</S.BtnLG> : <S.Btn>{this.props.text}</S.Btn>;
+    return this.props.lg ? (
+      <S.BtnLG onClick={this.props.handleClick}>{this.props.text}</S.BtnLG>
+    ) : (
+      <S.Btn onClick={this.props.handleClick}>{this.props.text}</S.Btn>
+    );
   }
 }
 
