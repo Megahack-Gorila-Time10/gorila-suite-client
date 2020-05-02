ead947516f622d9d9a9830177c2d00616013128a
Gabriel Zsigmond <gaazsig@gmail.com>
2020-05-02 02:15:10 -0300

    initialize global context

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -2,11 +2,27 @@ import React, { Component } from "react";
 import AppContext from "./AppContext";
 
 class AppProvider extends Component {
-  state = {};
+  state = {
+    currentSelect: 3,
+    allRes: [],
+  };
+
+  setCurrent = (value) => {
+    this.setState({ currentSelect: value });
+  };
+
+  handleNextQuestion = () => {
+    let newAllRes = this.state.allRes;
+    newAllRes.push(this.currentSelect);
+    this.setState({ allRes: newAllRes });
+    console.log("funfa");
+  };
 
   render() {
     const context = {
       state: this.state,
+      handleNextQuestion: this.handleNextQuestion,
+      setCurrent: this.setCurrent,
     };
     console.log(context.state);
 
