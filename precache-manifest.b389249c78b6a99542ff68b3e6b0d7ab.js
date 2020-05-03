d0b70368764b0736b324647dd7687a35af1ed0cf
Gabriel Zsigmond <gaazsig@gmail.com>
2020-05-03 03:11:03 -0300

    Updates

---
+++ precache-manifest.b389249c78b6a99542ff68b3e6b0d7ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d806ff3d4b74d23300fcf65ee53669f5",
+    "url": "/gorila-suite-client/index.html"
+  },
+  {
+    "revision": "481d094ff928bce1c936",
+    "url": "/gorila-suite-client/static/css/main.34a19829.chunk.css"
+  },
+  {
+    "revision": "d35922aad7bca5e406b3",
+    "url": "/gorila-suite-client/static/js/2.b3ae4fee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gorila-suite-client/static/js/2.b3ae4fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481d094ff928bce1c936",
+    "url": "/gorila-suite-client/static/js/main.5c70d220.chunk.js"
+  },
+  {
+    "revision": "90c29da9386e2c4ec923",
+    "url": "/gorila-suite-client/static/js/runtime-main.0a064c95.js"
+  }
+]);
